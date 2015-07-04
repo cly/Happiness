8f74675928fde39f44bb19059a56f346a75f340b
Charlie Liang Yuan <charlie@lyft.com>
2015-07-04 00:46:12 -0700

    finished happy face

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Charlie Yuan on 7/4/15.
+//  Copyright (c) 2015 Charlie Yuan. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+}
